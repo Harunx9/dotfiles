2419547e8d1f07251e9a027a5571ddacd356f291
Szymon <szwanot@gmail.com>
2021-10-15 19:14:00 +0200

    Fix nvim config]

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,82 +1,115 @@
-return require("packer").startup(function()
-	use({ "npxbr/glow.nvim", run = ":GlowInstall" })
-	use({ "TimUntersberger/neogit" })
-	use("wbthomason/packer.nvim")
-	use("navarasu/onedark.nvim")
+return (require("packer")).startup(function()
+	use({
+		"npxbr/glow.nvim",
+		run = ":GlowInstall"
+	});
+	use({
+		"TimUntersberger/neogit"
+	});
+	use("wbthomason/packer.nvim");
+	use("navarasu/onedark.nvim");
 	use({
 		"kyazdani42/nvim-tree.lua",
 		requires = "kyazdani42/nvim-web-devicons",
 		config = function()
-			require("nvim-tree").setup({})
-		end,
-	})
+			(require("nvim-tree")).setup({});
+		end
+	});
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
+			(require("nvim-treesitter.configs")).setup({
 				ensure_installed = "maintained",
 				highlight = {
 					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-			})
-		end,
-	})
+					additional_vim_regex_highlighting = false
+				}
+			});
+		end
+	});
 	use({
 		"neovim/nvim-lspconfig",
-		requires = { "kabouzeid/nvim-lspinstall", opt = true },
+		requires = {
+			"kabouzeid/nvim-lspinstall",
+			opt = true
+		},
 		config = function()
-			require("plugin_conf.lsp")
-		end,
-	})
+			require("plugin_conf.lsp");
+		end
+	});
 	use({
 		"hrsh7th/nvim-compe",
 		config = function()
-			require("plugin_conf.comple")
-		end,
-	})
+			require("plugin_conf.comple");
+		end
+	});
 	use({
 		"weilbith/nvim-code-action-menu",
-		cmd = "CodeActionMenu",
-	})
+		cmd = "CodeActionMenu"
+	});
 	use({
 		"akinsho/bufferline.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = require("bufferline").setup(),
-	})
+		requires = {
+			"kyazdani42/nvim-web-devicons",
+			opt = true
+		},
+		config = function()
+			(require("bufferline")).setup();
+		end
+	});
 	use({
 		"mhartington/formatter.nvim",
 		config = function()
-			require("plugin_conf.formatter")
-		end,
-	})
+			require("plugin_conf.formatter");
+		end
+	});
 	use({
 		"nvim-telescope/telescope.nvim",
 		config = function()
-			require("plugin_conf.telescope")
+			require("plugin_conf.telescope");
 		end,
-		requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
-	})
+		requires = {
+			{
+				"nvim-lua/popup.nvim"
+			},
+			{
+				"nvim-lua/plenary.nvim"
+			}
+		}
+	});
 	use({
 		"hoob3rt/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-		config = require("lualine").setup({
-			options = {
-				theme = "onedark",
-			},
-		}),
-	})
-	use({ "onsails/lspkind-nvim" })
-	use("lukas-reineke/indent-blankline.nvim")
-	use({ "wakatime/vim-wakatime" })
-	use({ "L3MON4D3/LuaSnip" })
-	use({ "glepnir/dashboard-nvim" })
+		requires = {
+			"kyazdani42/nvim-web-devicons",
+			opt = true
+		},
+		config = function()
+			(require("lualine")).setup({
+				options = {
+					theme = "onedark"
+				}
+			});
+		end
+	});
+	use({
+		"onsails/lspkind-nvim"
+	});
+	use("lukas-reineke/indent-blankline.nvim");
+	use({
+		"wakatime/vim-wakatime"
+	});
+	use({
+		"L3MON4D3/LuaSnip"
+	});
+	use({
+		"glepnir/dashboard-nvim"
+	});
 	use({
 		"akinsho/dependency-assist.nvim",
 		config = function()
-			require("dependency_assist").setup({})
-		end,
-	})
-	use("famiu/bufdelete.nvim")
-end)
+			(require("dependency_assist")).setup({});
+		end
+	});
+	use("famiu/bufdelete.nvim");
+end);
