2ae59f75c1a0c4cb795073f5716904a3634d706f
Szymon Wanot <szwanot@gmail.com>
2021-11-07 17:31:57 +0100

    Add nvim plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -158,4 +158,10 @@ return (require("packer")).startup(function()
 			})
 		end,
 	})
+	use({
+		"terrortylor/nvim-comment",
+		config = function()
+			require("nvim_comment").setup()
+		end,
+	})
 end)
