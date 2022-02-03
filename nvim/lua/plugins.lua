38356fec5d74065b6c0361a6867d36b7b852b68f
Szymon Wanot <szwanot@gmail.com>
2022-02-03 22:19:18 +0100

    fix fufferline config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -97,50 +97,6 @@ return (require("packer")).startup(function()
 					show_tab_indicators = true,
 					show_close_icon = false,
 				},
-				highlights = {
-					fill = {
-						guifg = { attribute = "fg", highlight = "Normal" },
-						guibg = { attribute = "bg", highlight = "StatusLineNC" },
-					},
-					background = {
-						guifg = { attribute = "fg", highlight = "Normal" },
-						guibg = { attribute = "bg", highlight = "StatusLine" },
-					},
-					buffer_visible = {
-						gui = "",
-						guifg = { attribute = "fg", highlight = "Normal" },
-						guibg = { attribute = "bg", highlight = "Normal" },
-					},
-					buffer_selected = {
-						gui = "",
-						guifg = { attribute = "fg", highlight = "Normal" },
-						guibg = { attribute = "bg", highlight = "Normal" },
-					},
-					separator = {
-						guifg = { attribute = "bg", highlight = "Normal" },
-						guibg = { attribute = "bg", highlight = "StatusLine" },
-					},
-					separator_selected = {
-						guifg = { attribute = "fg", highlight = "Special" },
-						guibg = { attribute = "bg", highlight = "Normal" },
-					},
-					separator_visible = {
-						guifg = { attribute = "fg", highlight = "Normal" },
-						guibg = { attribute = "bg", highlight = "StatusLineNC" },
-					},
-					close_button = {
-						guifg = { attribute = "fg", highlight = "Normal" },
-						guibg = { attribute = "bg", highlight = "StatusLine" },
-					},
-					close_button_selected = {
-						guifg = { attribute = "fg", highlight = "normal" },
-						guibg = { attribute = "bg", highlight = "normal" },
-					},
-					close_button_visible = {
-						guifg = { attribute = "fg", highlight = "normal" },
-						guibg = { attribute = "bg", highlight = "normal" },
-					},
-				},
 			})
 		end,
 	})
